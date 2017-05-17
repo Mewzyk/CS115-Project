dbde70022ca97884dc161d7909f90035623db92a
Justice <jdlammer@ucsc.edu>
2017-05-16 18:07:12 -0700

    Last commit was accident to show TA the develop branch (No changes in last or this commit)

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$90bbccf8f09e56e8$8de47c0bf0362ac035dfb31be51f1bdd42a8bc13"
+password="pbkdf2(1000,20,sha512)$aef1e4b003be5d1f$2759beaf6aeb1322fb2969bb204d18486bca74b6"
