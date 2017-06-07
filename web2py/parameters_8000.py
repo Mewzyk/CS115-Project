a77a93c839f88d8a3b362885beef14e62edd4c10
alex-hancock <alexbhancock@gmail.com>
2017-06-06 23:54:56 -0700

    Last commit. May God have mercy on our souls, all merge problems have finally been fixed.

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$aef1e4b003be5d1f$2759beaf6aeb1322fb2969bb204d18486bca74b6"
-=======
-password="pbkdf2(1000,20,sha512)$b91e7558178167a3$58b3234bed470c53e1598a5036bc1ee1d86443c8"
->>>>>>> RPSdevelop
+password="pbkdf2(1000,20,sha512)$a15e139ff011c9a8$46a3ea9b0087edf06349486a4321583ab4d71bac"
